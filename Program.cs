472d8f831622adaf638655a007b500537839289c
m.maier <m.maier@htlvb.at>
2024-11-22 09:18:25 +0100

    Changed ReadString to ReadOperator in Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ string @operator;
 Console.ForegroundColor = ConsoleColor.Yellow;
 
 operand1 = InputHelper.ReadNumber("Operand1: ");
-@operator = InputHelper.ReadString("Operator: ");
+@operator = InputHelper.ReadOperator("Operator: ");
 operand2 = InputHelper.ReadNumber("Operand2: ");
 
 
